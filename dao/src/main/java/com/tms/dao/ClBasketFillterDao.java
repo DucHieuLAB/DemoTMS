a0ab2b1cf6a4422a504eba96bf1253a27d5e3fd5
Dev_DucHieu <duchieu.dev@gmail.com>
2023-09-07 17:52:12 +0700

    [fear] Update Lead

--- dao/src/main/java/com/tms/dao/ClBasketFillterDao.java
+++ dao/src/main/java/com/tms/dao/ClBasketFillterDao.java
@@ -2,6 +2,7 @@ package com.tms.dao;
 
 import com.tms.commons.DBResponse;
 import com.tms.dto.request.schedule.GetLeadToFillter;
+import com.tms.dto.request.schedule.UpdLeadFillter;
 import com.tms.dto.response.LeadBasket;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +11,17 @@ import java.util.List;
 @Service
 public class ClBasketFillterDao extends BaseDao{
     private static final String GET_NULL_ATTRIBUTE3 = "get_null_attribute3_rows";
+    private static final String UPDATE_LEAD_FILLTER = "update_lead_fillter";
 
     /* BEGIN GET */
     public DBResponse<List<LeadBasket>> getLeadUpdate(String sessionId , GetLeadToFillter params) {
         return this.dbGet(sessionId, GET_NULL_ATTRIBUTE3, params, LeadBasket.class);
     }
     /* END GET */
+
+    /* BEGIN UPDATE */
+    public DBResponse<String> updLeadFillter(String sessionId, UpdLeadFillter params) {
+        return this.dbInsOrUpd(sessionId, UPDATE_LEAD_FILLTER,params);
+    }
+    /* END UPDATE */
 }
--- src/main/java/com/tms/api/service/FillterLeadService.java
+++ src/main/java/com/tms/api/service/FillterLeadService.java
@@ -1,12 +1,17 @@
 package com.tms.api.service;
 
+import com.tms.api.exception.TMSException;
 import com.tms.dto.request.schedule.GetLeadToFillter;
+import com.tms.dto.request.schedule.UpdLeadFillter;
 import com.tms.dto.response.LeadBasket;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public interface FillterLeadService {
+
     public void fillterLead() ;
     public List<LeadBasket> getListToFillter(GetLeadToFillter getLeadToFillter);
+
+    Boolean updLeadFillter(UpdLeadFillter updLeadFillter) throws TMSException;
 }
