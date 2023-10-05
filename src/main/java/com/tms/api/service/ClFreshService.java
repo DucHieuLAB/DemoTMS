40fcadc4c6efee214e8ecce3a9e4857b2854ad18
truong <truong.nv.1929@aptechlearning.edu.vn>
2023-10-05 15:31:42 +0700

    5/10 331

--- src/main/java/com/tms/api/service/ClFreshService.java
+++ src/main/java/com/tms/api/service/ClFreshService.java
@@ -2,14 +2,22 @@ package com.tms.api.service;
 
 import com.tms.api.exception.TMSDbException;
 import com.tms.api.exception.TMSException;
+import com.tms.dto.request.ClFreshGetLead.GetLeadfor;
+import com.tms.dto.request.ClFreshGetLead.SetLeadStatus;
 import com.tms.dto.request.clFresh.InsClFresh;
 import com.tms.dto.request.clFresh.UpdClFresh;
 import com.tms.dto.request.schedule.UpdScheduleUpdate;
+import com.tms.dto.response.GetLeadForAgentDto;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public interface ClFreshService {
     public void insertClFresh(List<InsClFresh> clFreshes, String sessionId) throws TMSDbException;
 
     boolean updClFreshAfterValidSO(int leadId, UpdClFresh updClFresh) throws TMSDbException;
+    
+    List<GetLeadForAgentDto> getLeadForAgent(GetLeadfor getLeadfor) throws TMSException;
+
+    boolean updLead(int id,SetLeadStatus setLeadStatus) throws TMSException, IllegalAccessException, InvocationTargetException, NoSuchMethodException;
 }
