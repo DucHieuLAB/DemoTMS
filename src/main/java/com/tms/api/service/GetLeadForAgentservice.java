0ca8c52f7fc9f144c2b37eb23a409d23e8f3a1ca
truong <truong.nv.1929@aptechlearning.edu.vn>
2023-10-05 13:20:27 +0700

    [update] 5/10 1319

--- src/main/java/com/tms/api/service/GetLeadForAgentservice.java
+++ src/main/java/com/tms/api/service/GetLeadForAgentservice.java
@@ -7,7 +7,7 @@ import com.tms.dto.request.ClFreshGetLead.GetLeadfor;
 import com.tms.dto.request.ClFreshGetLead.SetLeadStatus;
 import com.tms.dto.response.GetLeadForAgentDto;
 public interface GetLeadForAgentService { 
-    
     List<GetLeadForAgentDto> getLeadForAgent(GetLeadfor getLeadfor) throws TMSException;
-        
+    boolean updLead(int id,SetLeadStatus setLeadStatus) throws  TMSException, IllegalAccessException, InvocationTargetException, NoSuchMethodException;
+    
 }
