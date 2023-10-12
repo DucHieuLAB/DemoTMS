14fbe0e842bc1f3fb3632bded216bc43292b9875
Dev_DucHieu <duchieu.dev@gmail.com>
2023-10-12 15:28:35 +0700

    TMS-5

--- src/main/java/com/tms/api/service/ClBasketService.java
+++ src/main/java/com/tms/api/service/ClBasketService.java
@@ -1,6 +1,5 @@
 package com.tms.api.service;
 
-import com.tms.api.exception.TMSDbException;
 import com.tms.api.exception.TMSException;
 import com.tms.dto.request.clBasket.GetLeadToFillter;
 import com.tms.dto.response.ClBasket;
@@ -14,9 +13,9 @@ public interface ClBasketService {
 
     List<ClBasket> getListToFillter(GetLeadToFillter getLeadToFillter) throws TMSException;
 
-    List<ClBasket> getLeadInTimeRange(LocalDateTime StartTime, LocalDateTime EndTime) throws TMSException;
+    List<ClBasket> getLeadInTimeRange(LocalDateTime startTime, LocalDateTime endTime) throws TMSException;
 
     List<ClBasket> getListToFilterProcess(String sessionId) throws TMSException;
 
-    void updateClBasket(List<ClBasket> clBaskets, String sessionId, String TimeZone) throws TMSException;
+    void updateClBasket(List<ClBasket> clBaskets, String sessionId, String timeZone) throws TMSException;
 }
